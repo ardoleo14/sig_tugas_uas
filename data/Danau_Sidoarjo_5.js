b49fac974a85ef341657efa905d2da548f03642a
ardoleo14 <ardoleo07@gmail.com>
2026-01-10 22:40:24 +0700

    Add files via upload

---
+++ data/Danau_Sidoarjo_5.js
@@ -0,0 +1,1 @@
+var json_Danau_Sidoarjo_5 = {"type":"FeatureCollection","name":"Danau_Sidoarjo_5","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ data/Sungai_Besar_Sidoarjo_8.js
@@ -0,0 +1,1 @@
+var json_Sungai_Besar_Sidoarjo_8 = {"type":"FeatureCollection","name":"Sungai_Besar_Sidoarjo_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
